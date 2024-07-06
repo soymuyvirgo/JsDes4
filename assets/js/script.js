a450236a7f5d498b3affd04572ad906a0edfac18
Alexander Letzkus <alexanderletzkus@gmail.com>
2024-07-06 18:03:25 +0200

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,191 @@
+const propiedades_venta = [
+    {
+        nombre: 'Apartamento de lujo en zona exclusiva',
+        src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+        descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+        ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+        habitaciones: 4,
+        costo: 5000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Apartamento acogedor en la montaña',
+        src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+        descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+        ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
+        habitaciones: 2,
+        costo: 1200,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Penthouse de lujo con terraza panorámica',
+        src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+        descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+        ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
+        habitaciones: 3,
+        costo: 4500,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Apartamento de lujo en zona exclusiva',
+        src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+        descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+        ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+        habitaciones: 4,
+        costo: 5000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Apartamento acogedor en la montaña',
+        src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+        descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+        ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
+        habitaciones: 2,
+        costo: 1200,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Penthouse de lujo con terraza panorámica',
+        src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+        descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+        ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
+        habitaciones: 3,
+        costo: 4500,
+        smoke: false,
+        pets: true
+    }
+
+];
+
+const propiedades_alquiler = [
+    {
+        nombre: 'Apartamento en el centro de la ciudad',
+        src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        costo: 2000,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Apartamento luminoso con vista al mar',
+        src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+        habitaciones: 3,
+        costo: 2500,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Condominio moderno en zona residencial',
+        src: 'imagen7.jpg',
+        descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        costo: 2200,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Casa de campo con jardín amplio',
+        src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        descripcion: 'Hermosa casa de campo con un jardín amplio y vistas a la naturaleza',
+        ubicacion: '456 Country Lane, Greenfield, CA 78945',
+        habitaciones: 5,
+        costo: 3500,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Apartamento en el centro de la ciudad',
+        src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        costo: 2000,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Apartamento luminoso con vista al mar',
+        src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+        habitaciones: 3,
+        costo: 2500,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Condominio moderno en zona residencial',
+        src: 'imagen7.jpg',
+        descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        costo: 2200,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Casa de campo con jardín amplio',
+        src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        descripcion: 'Hermosa casa de campo con un jardín amplio y vistas a la naturaleza',
+        ubicacion: '456 Country Lane, Greenfield, CA 78945',
+        habitaciones: 5,
+        costo: 3500,
+        smoke: true,
+        pets: true
+    }
+];
+
+
+function generarPropiedadesHTML(propiedades, containerId) {
+    const container = document.getElementById(containerId);
+    let html = '';
+
+    propiedades.forEach(propiedad => {
+        html += `
+            <div class="propiedad">
+                <img src="${propiedad.src}" alt="${propiedad.nombre}">
+                <h4>${propiedad.nombre}</h4>
+                <p>${propiedad.descripcion}</p>
+                <p><strong>Ubicación:</strong> ${propiedad.ubicacion}</p>
+                <p><strong>Habitaciones:</strong> ${propiedad.habitaciones} | <strong>Baños:</strong> ${propiedad.banos}</p>
+                <p><strong>Costo:</strong> $${propiedad.costo}</p>
+                <p>${propiedad.smoke ? '✅ Permitido fumar' : '🚭 No se permite fumar'}</p>
+                <p>${propiedad.pets ? '🐾 Mascotas permitidas' : '🚫 No se permiten mascotas'}</p>
+            </div>
+        `;
+    });
+
+    container.innerHTML = html;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+            // Para mostrar solo las primeras 3 propiedades en la página principal
+    if (document.getElementById('propiedades-venta')) {
+
+        const propiedadesVentaMostrar = propiedades_venta.slice(0, 3);
+        generarPropiedadesHTML(propiedadesVentaMostrar, 'propiedades-venta');
+    }
+
+    if (document.getElementById('propiedades-alquiler')) {
+        const propiedadesAlquilerMostrar = propiedades_alquiler.slice(0, 3);
+        generarPropiedadesHTML(propiedadesAlquilerMostrar, 'propiedades-alquiler');
+    }
+
+    if (document.URL.includes('venta.html')) {
+        generarPropiedadesHTML(propiedades_venta, 'propiedades-venta');
+    }
+
+    if (document.URL.includes('alquiler.html')) {
+        generarPropiedadesHTML(propiedades_alquiler, 'propiedades-alquiler');
+    }
+});
